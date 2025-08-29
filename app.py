03eb93d232465ef1515d8a4f3c6e5f152330f807
vijayaraj Maddala <maddalavijayaraj@gmail.com>
2025-08-29 19:42:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
+app.py 
 from flask import Flask, render_template 
-app=Flask(__name__) 
+app = Flask(__name__) 
 @app.route('/') 
 def index(): 
-    """Renders the main index.html template for the Smart Register applica on.""" 
+    """ 
+    Renders the main index.html template for the Smart Register applica on. 
+    """ 
     return render_template('index.html') 
- if __name__=='__main__': 
+ 
+if __name__ == '__main__': 
     # Run the Flask applica on in debug mode for development 
-    app.run(debug=True) 
+    app.run(debug=True)
